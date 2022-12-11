a733244c200ac265085dcdaeca17eae9023c2552
LiJiun <buzy89456@gmail.com>
2022-12-11 15:20:51 +0800

    crawler2

---
+++ crawler/crawler2.js
@@ -0,0 +1,21 @@
+const axios = require("axios");
+
+// axios
+//   .get("http://54.71.133.152:3000/stocks?stockNo=2618&date=202211")
+//   .then((response) => {
+//     console.log(response.data);
+//   })
+//   .catch((e) => {
+//     console.error(e);
+//   });
+
+(async () => {
+  try {
+    let response = await axios.get(
+      "http://54.71.133.152:3000/stocks?stockNo=2618&date=202211"
+    );
+    console.log(response.data);
+  } catch (e) {
+    console.error(e);
+  }
+})();
